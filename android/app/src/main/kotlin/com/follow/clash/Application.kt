ff704f6f372d0a4fcdca92630254358725ba6197
LM-Firefly <firefly.lzh@gmail.com>
2025-09-07 09:30:41 +0800

    build: 移除 Firebase 相关配置
    - 从 build.gradle.kts 中移除 Firebase 插件和依赖
    - 删除 google-services.json 文件
    - 从 Application 类中移除 Firebase 初始化代码
    - 更新 libs.versions.toml，移除 Firebase 相关版本
    - 从 settings.gradle.kts 中移除 Firebase 插件

--- android/app/src/main/kotlin/com/follow/clash/Application.kt
+++ android/app/src/main/kotlin/com/follow/clash/Application.kt
@@ -4,8 +4,6 @@ import android.app.Application
 import android.content.Context
 import com.follow.clash.common.GlobalState
 import com.follow.clash.common.processName
-import com.google.firebase.FirebaseApp
-import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class Application : Application() {
 
@@ -16,18 +14,6 @@ class Application : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        initRemoteCrashlytics(this)
-    }
-
-    private fun initRemoteCrashlytics(context: Context) {
-        try {
-            if (processName?.endsWith(":remote") == true) {
-                FirebaseApp.initializeApp(context)
-                FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled = true
-                GlobalState.log("init remote crashlytics")
-            }
-        } catch (e: Exception) {
-            GlobalState.log("initRemoteCrashlytics error: $e")
-        }
+        GlobalState.log("Application started without Firebase")
     }
 }
